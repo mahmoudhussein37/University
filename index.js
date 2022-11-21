917c60b5dafed3365e36258342b77943dce1106c
mahmoudhussein37 <112219631+mahmoudhussein37@users.noreply.github.com>
2022-11-21 15:37:15 +0200

    I create two variables to calculate the correct distance between any point from page to left or top of the button .

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const btnEl = document.querySelector(".btn");
 btnEl.addEventListener("mouseover", (event)=>{
-
+    const x = (event.pageX - btnEl.offsetLeft);
+    const y = (event.pageY - btnEl.offsetTop);
 })
